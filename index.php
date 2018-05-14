40568ab96cb83a284ac5f02d3e96b35f7e85984b
AtomJump <peter@atomjump.com>
2018-05-14 14:33:17 +1200

    installer for db version

--- index.php
+++ index.php
@@ -95,7 +95,7 @@
     
     function run_query($sql, $json, $no_results_msg = null) 
     {
-    	error_log("Trying to connect to database: " . json_encode($json));
+    	error_log("Trying to connect to database: " . $json['dbname']);
     	
 		if(odbc_db_code_checks($json)) {
 
